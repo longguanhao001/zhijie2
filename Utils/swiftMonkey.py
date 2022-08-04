94163138c4509090f8ffb27c31b955dc5c0cf33a
huangzj <huangzj@flatincbr.com>
2022-08-04 17:00:07 +0800

    monkey

--- Utils/swiftMonkey.py
+++ Utils/swiftMonkey.py
@@ -1,10 +1,15 @@
 import os
 
 if __name__ == '__main__':
-  	#执行monkey
-    result = os.popen("xcodebuild -project /Users/vanced/Downloads/sjk_swiftmonkey/sjk-monkey.xcodeproj -scheme sjk-monkey -destination 'id=20a7adaffd52ebb0f01efea599592e4272297911' test")
-    print(result)
-    #记录日志
-    #记录截图
-    #扫描崩溃
-    #上报dingding+提bug
+  	#   执行monkey
+    # 记录日志
+    # 扫描崩溃
+    fileList = []
+    path = "/Users/vanced/Downloads/crashreport"
+    for root, dirs, files in os.walk("../", topdown=False):
+        for f in files:
+            if "PureTuber" in f:
+                fileList.append(f)
+    print(fileList)
+    # 上报dingding+提bug
+    # 记录截图
