3eea2f96441ef5d9628373f2e418f9ea08bfe04d
huangzj <huangzj@flatincbr.com>
2022-08-04 17:33:25 +0800

    monkey

--- Utils/swiftMonkey.py
+++ Utils/swiftMonkey.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 if __name__ == '__main__':
   	#   执行monkey
@@ -6,9 +7,10 @@ if __name__ == '__main__':
     # 扫描崩溃
     fileList = []
     path = "/Users/vanced/Downloads/crashreport"
+    date_str = time.strftime('%m-%d', time.localtime())
     for root, dirs, files in os.walk(path, topdown=False):
         for f in files:
-            if "PureTuber" in f:
+            if "PureTuber" in f and date_str in f:
                 fileList.append(f)
     print(fileList)
     # 上报dingding+提bug
