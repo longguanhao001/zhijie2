16735ff467c2bf89cecfad365275fa41caf1ed2f
huangzj <huangzj@flatincbr.com>
2022-08-04 11:54:58 +0800

    ios monkey

--- Utils/swiftMonkey.py
+++ Utils/swiftMonkey.py
@@ -1,8 +1,9 @@
 import os
 
 if __name__ == '__main__':
-    result = os.popen("")
-    #执行monkey
+  	#执行monkey
+    result = os.popen("xcodebuild -project /Users/vanced/Downloads/sjk_swiftmonkey/sjk-monkey.xcodeproj -scheme sjk-monkey -destination 'id=20a7adaffd52ebb0f01efea599592e4272297911' test")
+    print(result)
     #记录日志
     #记录截图
     #扫描崩溃
