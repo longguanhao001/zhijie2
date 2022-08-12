91c09e4d828735cc15cf2ed45568e1d2878c7954
huangzj <huangzj@flatincbr.com>
2022-08-12 10:23:49 +0800

    断言优化

--- Utils/Assert.py
+++ Utils/Assert.py
@@ -10,10 +10,10 @@ class Assert():
     def assert_not_equal(self, expect, actul):
         assert actul != expect, "{0}等于{1}".format(actul, expect)
 
-    def assert_in(self, expect, actul):
+    def assert_in(self, actul, expect):
         assert actul in expect, "{0}不包含{1}".format(expect, actul)
 
-    def assert_not_in(self, expect, actul):
+    def assert_not_in(self, actul, expect):
         assert actul not in expect, "{0}包含{1}".format(expect, actul)
 
     def assert_less(self, expect, actul):
@@ -22,8 +22,8 @@ class Assert():
     def assert_greater(self, expect, actul):
         assert actul > expect, "{0}大于{1}".format(expect, actul)
 
-    def assert_greaterAndEqual(self, expect, actul):
+    def assert_greaterAndEqual(self, actul, expect):
         assert actul >= expect, "{0}大于等于{1}".format(expect, actul)
 
-    def assert_lessAndEqual(self, expect, actul):
+    def assert_lessAndEqual(self, actul, expect):
         assert actul <= expect, "{0}小于等于{1}".format(expect, actul)
